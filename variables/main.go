1753ce850db86d6bd75dc965012ef295cc8237ef
Samir Azimov <samirazimov488@gmail.com>
2026-01-22 03:34:48 +0500

    Create main.go

---
+++ variables/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var b , c int = 1,2
+	fmt.Println(b,c)
+
+	var name , lastname  string = "pavel" , "durov"
+	fmt.Println(name , lastname)
+
+	token := "myprotectedtoken"
+	fmt.Println(token)
+
+	var username string
+	username = "Alex"
+	fmt.Println("Welcome ", username)
+
+
+
+	var age = 19
+	fmt.Println(age)
+
+
+	var d = true 
+	fmt.Println(d)
+}
